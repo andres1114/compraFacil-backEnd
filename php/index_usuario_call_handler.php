f4c4b91f002e7be1ad745104d8b4de7711276dc1
Andres Zapata <polako_1114@hotmail.com>
2021-11-05 19:36:49 -0500

    Adds mainladning page backend code to search for products

--- php/index_usuario_call_handler.php
+++ php/index_usuario_call_handler.php
@@ -2,7 +2,8 @@
 
     $session_handler_include_origin = "back_end";
     $session_handler_permission_code = 2;
-    include_once "session_handler.php";
+    //include_once "session_handler.php";
+    include_once "db_connection.php";
 
     $json_response = Array();
 
@@ -19,13 +20,49 @@
 		$pdo_mysql_rw = pdoCreateConnection(array('db_type' => "mysql", 'db_host' => "localhost", 'db_user' => "root", 'db_pass' => 'admin', 'db_name' => "compraFacil"));
 
         switch ($json_data->callHeader) {
-            case "closeSession":
+            case "searchProduct":
 
                 $query_args = array(
-                    "cookieid" => $json_data->callArguments->cookieId
+                    "productName" => "%".preg_replace("/\s/", "%", $json_data->callArguments->productName)."%"
                 );
-                $query = "DELETE FROM session WHERE session_id = :cookieid";
-                pdoExecuteQuery($pdo_mysql_rw,$query,$query_args,$json_data->callHeader."_query_01");
+                $query = "SELECT 
+                        pr.id, 
+                        al.nombre_almacen, 
+                        pr.nombre_producto, 
+                        pr.precio_producto,
+                        CONCAT(i1.index_archivo,'.',i1.extencion_archivo) AS imagen_almacen,
+                        CONCAT(i2.index_archivo,'.',i2.extencion_archivo) AS imagen_producto
+                    FROM producto AS pr 
+                        INNER JOIN almacen AS al 
+                            ON al.id = pr.id_almacen
+                        LEFT JOIN imagen i1 
+                            ON al.id_imagen = i1.id
+                        LEFT JOIN imagen i2 
+                            ON pr.id_imagen = i2.id
+                    WHERE
+                        pr.nombre_producto LIKE :productName
+                    AND 
+                        pr.activo IS TRUE  
+                    ORDER BY 
+                        pr.precio_producto ASC";
+
+                $query_data = pdoExecuteQuery($pdo_mysql_rw,$query,$query_args,$json_data->callHeader."_query_01");
+
+                $json_response["values"] = array(
+                    "groceryName" => array()
+                    ,"productName" => array()
+                    ,"productPrize" => array()
+                    ,"productImage" => array()
+                    ,"groceryImage" => array()
+                );
+
+                for ($x = 0; $x < $query_data[1]; $x++) {
+                    $json_response["values"]["groceryName"][$x] = $query_data[0][$x]["nombre_almacen"];
+                    $json_response["values"]["productName"][$x] = $query_data[0][$x]["nombre_producto"];
+                    $json_response["values"]["productPrize"][$x] = $query_data[0][$x]["precio_producto"];
+                    $json_response["values"]["productImage"][$x] = $query_data[0][$x]["imagen_producto"];
+                    $json_response["values"]["groceryImage"][$x] = $query_data[0][$x]["imagen_almacen"];
+                }
 
                 $json_response["statusCode"] = 200;
                 $json_response["errorMessage"] = null;
